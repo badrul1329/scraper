a5a944eb91f2a22cd540eebfec21babd3b453522
badrul1329 <badrul15-1329@diu.edu.bd>
2016-06-25 16:02:10 +0600

    Successfully retrived the single post reviews

--- laraScrap/app/Http/Controllers/homeController.php
+++ laraScrap/app/Http/Controllers/homeController.php
@@ -25,49 +25,70 @@ class homeController extends Controller {
 //            }
 //        }
 //        $result['categories'] = $categories;
-
 //get hotelinfo from category
-        $url = 'https://www.tripadvisor.com/Hotels-g29092-Anaheim_California-Hotels.html';
+//        $url = 'https://www.tripadvisor.com/Hotels-g29092-Anaheim_California-Hotels.html';
+//
+//        $domain = parse_url($url, PHP_URL_HOST);
+//
+//        do {
+//            $html = HtmlDomParser::file_get_html($url);
+//            $ret = $html->find('div[class=listing_info]');
+//            foreach ($ret as $div) {
+//
+//                $hotelName = $div->find('div[class=listing_title]', 0)->first_child()->plaintext;
+//                $hotelLink = 'https://' . $domain . $div->find('div[class=listing_title]', 0)->first_child()->href;
+//                if (!empty($div->find('[class=more]', 0)->plaintext)) {
+//                    $hotelReviews = $div->find('[class=more]', 0)->plaintext;
+//                } else {
+//                    $hotelReviews = '0 Reviews';
+//                }
+//                if (!empty($div->find('img[class=sprite-ratings]', 0)->alt)) {
+//                    $hotelRate = $div->find('img[class=sprite-ratings]', 0)->alt;
+//                } else {
+//                    $hotelRate = '0 of 5 stars';
+//                }
+//                $Tags = array();
+//                foreach ($div->find('a[class=tag]') as $data) {
+//                    $Tags[] = $data->plaintext;
+//                }
+//                if(count($Tags)>0){
+//                    $hotelTags = $Tags;
+//                }else{
+//                    $hotelTags = 'No Tags';
+//                }
+//                
+//                unset($Tags);
+//                $hotels[] = array('hotelName' => $hotelName, 'hotelLink' => $hotelLink, 'hotelReviews' => $hotelReviews, 'hotelRate' => $hotelRate, 'hotelTags' => $hotelTags);
+//            }
+//
+//            if (!empty($html->find('div[class=pagination]', 0)->childNodes(1)->href)) {
+//                $url = 'https://' . $domain . $html->find('div[class=pagination]', 0)->childNodes(1)->href;
+//            }
+//        } while (!empty($html->find('div[class=pagination]', 0)->childNodes(1)->href));
+//
+//        $result['hotels'] = $hotels;
 
-        $domain = parse_url($url, PHP_URL_HOST);
-
+        
+//get hotel review for specific hotel
+        $url = 'https://www.tripadvisor.com/Hotel_Review-g29092-d1219838-Reviews-Anaheim_Hacienda_Inn_Suites_Disneyland-Anaheim_California.html';
+        $domain = str_ireplace('www.', '', parse_url($url, PHP_URL_HOST));
         do {
             $html = HtmlDomParser::file_get_html($url);
-            $ret = $html->find('div[class=listing_info]');
-            foreach ($ret as $div) {
+            $ret = $html->find('div[class=reviewSelector]');
+            foreach ($ret as $data) {
+                $title = $data->find('div.quote', 0)->plaintext;
+                $link = 'https://'.$domain.$data->find('div.quote', 0)->first_child()->href;
+                $reviewhtml = HtmlDomParser::file_get_html($link);
+                $body = $reviewhtml->find('div[class=reviewSelector]',0)->find('div[class=entry]', 0)->first_child()->plaintext;
 
-                $hotelName = $div->find('div[class=listing_title]', 0)->first_child()->plaintext;
-                $hotelLink = 'https://' . $domain . $div->find('div[class=listing_title]', 0)->first_child()->href;
-                if (!empty($div->find('[class=more]', 0)->plaintext)) {
-                    $hotelReviews = $div->find('[class=more]', 0)->plaintext;
-                } else {
-                    $hotelReviews = '0 Reviews';
-                }
-                if (!empty($div->find('img[class=sprite-ratings]', 0)->alt)) {
-                    $hotelRate = $div->find('img[class=sprite-ratings]', 0)->alt;
-                } else {
-                    $hotelRate = '0 of 5 stars';
-                }
-                $Tags = array();
-                foreach ($div->find('a[class=tag]') as $data) {
-                    $Tags[] = $data->plaintext;
-                }
-                if(count($Tags)>0){
-                    $hotelTags = $Tags;
-                }else{
-                    $hotelTags = 'No Tags';
-                }
-                
-                unset($Tags);
-                $hotels[] = array('hotelName' => $hotelName, 'hotelLink' => $hotelLink, 'hotelReviews' => $hotelReviews, 'hotelRate' => $hotelRate, 'hotelTags' => $hotelTags);
+                 $reviews[] = array('title' => $title,'body' => $body);
             }
-
-            if (!empty($html->find('div[class=pagination]', 0)->childNodes(1)->href)) {
-                $url = 'https://' . $domain . $html->find('div[class=pagination]', 0)->childNodes(1)->href;
+            if ($next = $html->find('a[class=next]', 0)) {
+                $url = 'https://www.' . $domain . $next->href;
             }
-        } while (!empty($html->find('div[class=pagination]', 0)->childNodes(1)->href));
+        } while ($html->find('a[class=next]', 0));
 
-        $result['hotels'] = $hotels;
+        $result['reviews'] = $reviews;
 
         return view('scraper', compact('result'));
     }
