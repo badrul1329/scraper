adaecb1a641b5827a142a3c405f556c4cffaca5c
badrul1329 <badrul15-1329@diu.edu.bd>
2016-06-25 14:31:57 +0600

    Hotelinfo from specific category Successfully Retrieved

--- laraScrap/app/Http/Controllers/homeController.php
+++ laraScrap/app/Http/Controllers/homeController.php
@@ -28,39 +28,47 @@ class homeController extends Controller {
 
 //get hotelinfo from category
         $url = 'https://www.tripadvisor.com/Hotels-g29092-Anaheim_California-Hotels.html';
-        //$url = 'https://www.tripadvisor.com/Hotels-g29092-oa60-Anaheim_California-Hotels.html#ACCOM_OVERVIEW';
-//        $url = 'https://www.tripadvisor.com/Hotels-g29092-oa90-Anaheim_California-Hotels.html#ACCOM_OVERVIEW';
 
         $domain = parse_url($url, PHP_URL_HOST);
-        
-        do{
+
+        do {
             $html = HtmlDomParser::file_get_html($url);
             $ret = $html->find('div[class=listing_info]');
             foreach ($ret as $div) {
+
                 $hotelName = $div->find('div[class=listing_title]', 0)->first_child()->plaintext;
                 $hotelLink = 'https://' . $domain . $div->find('div[class=listing_title]', 0)->first_child()->href;
-                $hotelReviews = $div->find('[class=more]', 0)->plaintext;
-                $hotelRate = $div->find('img[class=sprite-ratings]', 0)->alt;
+                if (!empty($div->find('[class=more]', 0)->plaintext)) {
+                    $hotelReviews = $div->find('[class=more]', 0)->plaintext;
+                } else {
+                    $hotelReviews = '0 Reviews';
+                }
+                if (!empty($div->find('img[class=sprite-ratings]', 0)->alt)) {
+                    $hotelRate = $div->find('img[class=sprite-ratings]', 0)->alt;
+                } else {
+                    $hotelRate = '0 of 5 stars';
+                }
                 $Tags = array();
                 foreach ($div->find('a[class=tag]') as $data) {
                     $Tags[] = $data->plaintext;
                 }
-                $hotelTags = $Tags;
+                if(count($Tags)>0){
+                    $hotelTags = $Tags;
+                }else{
+                    $hotelTags = 'No Tags';
+                }
+                
                 unset($Tags);
                 $hotels[] = array('hotelName' => $hotelName, 'hotelLink' => $hotelLink, 'hotelReviews' => $hotelReviews, 'hotelRate' => $hotelRate, 'hotelTags' => $hotelTags);
             }
-            if(!empty($html->find('div[class=pagination]',0)->childNodes(1)->href)){
-                $url='https://'.$domain.$html->find('div[class=pagination]',0)->childNodes(1)->href;
-                echo $url."<br/>";
+
+            if (!empty($html->find('div[class=pagination]', 0)->childNodes(1)->href)) {
+                $url = 'https://' . $domain . $html->find('div[class=pagination]', 0)->childNodes(1)->href;
             }
-        }while(!empty($html->find('div[class=pagination]',0)->childNodes(1)->href));
+        } while (!empty($html->find('div[class=pagination]', 0)->childNodes(1)->href));
+
         $result['hotels'] = $hotels;
-        
 
-//        $result['link'] = $html->find('div[class=pagination]',0)->childNodes(1)->href;
-        $result['link'] = $html->find('a[class=next]',0);
-
-        
         return view('scraper', compact('result'));
     }
 
